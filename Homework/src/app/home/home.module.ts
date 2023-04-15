23cb1fb7e7c179415c92c92b67e756c2cd0644e3
Robert <enescurobert88@gmail.com>
2023-04-15 17:15:28 +0300

    App V2 (cu services)

--- Homework/src/app/home/home.module.ts
+++ Homework/src/app/home/home.module.ts
@@ -6,9 +6,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
 @NgModule({
-  declarations: [DashboardComponent, CardsComponent, HeaderComponent, MenuBarComponent],
+  declarations: [
+    DashboardComponent,
+    CardsComponent,
+    HeaderComponent,
+    MenuBarComponent,
+    OurServicesComponent,
+  ],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
--- Homework/src/app/services/card.service.ts
+++ Homework/src/app/services/card.service.ts
@@ -8,37 +8,37 @@ import { Card } from '../interfaces/card.interface';
 export class CardService {
   cards: Card[] = [
     {
-      title: 'Card 1',
+      title: 'Stockholm',
       imageUrl:
         'https://images.unsplash.com/photo-1626593261859-4fe4865d8cb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&w=1000&q=80',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      title: 'Card 2',
+      title: 'Tokyo',
       imageUrl:
         'https://cdn.pixabay.com/photo/2022/08/28/01/40/cyberpunk-city-7415576__480.jpg',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      title: 'Card 3',
+      title: 'Vienna',
       imageUrl:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCp5sSmiwGsib1rHV_8_LgACqKO-qiQCDeSA&usqp=CAU',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      title: 'Card 4',
+      title: 'London',
       imageUrl:
         'https://apprendre-la-photo.fr/wp-content/uploads/2015/09/stockholm.jpg',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      title: 'Card 5',
+      title: 'New Delhi',
       imageUrl:
         'https://static-cse.canva.com/blob/825907/ComposeStunningImages7.jpg',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      title: 'Card 6',
+      title: 'Dubai',
       imageUrl:
         'https://c4.wallpaperflare.com/wallpaper/372/912/846/3-316-16-9-aspect-ratio-s-sfw-wallpaper-preview.jpg',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
